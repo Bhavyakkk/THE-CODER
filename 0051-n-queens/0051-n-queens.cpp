e0c49e09a2f4929f9b82ae31c01792f7418996a8
THE CODER <bhavyaaaaa727@gmail.com>
2025-06-17 23:12:59 +0530

    Time: 0 ms (100%), Space: 10.2 MB (55.26%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    
+    bool canplace(int row, int col, vector<string>& board, int n) {
+        int ro = row, co = col;
+
+        
+        while (ro >= 0 && co >= 0) {
+            if (board[ro--][co--] == 'Q') return false;
+        }
+
+        
+        ro = row; co = col;
+        while (ro < n && co >= 0) {
+            if (board[ro++][co--] == 'Q') return false;
+        }
+
+        
+        ro = row; co = col;
+        while (co >= 0) {
+            if (board[ro][co--] == 'Q') return false;
+        }
+
+        return true;
+    }
+
+    
+    void solve(int col, vector<vector<string>>& ans, vector<string>& board, int n) {
+        if (col == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (canplace(row, col, board, n)) {
+                board[row][col] = 'Q';
+                solve(col + 1, ans, board, n);
+                board[row][col] = '.';  
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.')); 
+        solve(0, ans, board, n);
+        return ans;
+    }
+};
