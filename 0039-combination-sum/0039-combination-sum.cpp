70fa57b3eb3b3d83cb24fcfd646932898f8aab7f
THE CODER <bhavyaaaaa727@gmail.com>
2025-06-26 16:59:14 +0530

    Time: 4 ms (43.6%), Space: 16.9 MB (34.91%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+set<vector<int>>s;
+
+void getallcombinations(vector<int> &arr, int idx, int tar, vector<vector<int>> &ans, vector<int> &combin){
+    if(idx==arr.size() || tar<0){
+        return;
+    }
+    if(tar==0){
+        if(s.find(combin)==s.end()){
+            ans.push_back(combin);
+            s.insert(combin);
+        }
+        return;
+    }
+    combin.push_back(arr[idx]);
+    getallcombinations(arr,idx+1,tar-arr[idx],ans,combin);
+    getallcombinations(arr,idx,tar-arr[idx],ans,combin);
+    combin.pop_back();
+
+    getallcombinations(arr,idx+1,tar,ans,combin);
+}
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+        vector<vector<int>> ans;
+        vector<int> combin;
+
+        getallcombinations(arr,0,target,ans,combin);
+        
+        return ans;
+    }
+};
