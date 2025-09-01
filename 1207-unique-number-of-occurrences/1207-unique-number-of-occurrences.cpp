007be3166a4b54bc21a4aaa32364a573a22b882b
THE CODER <bhavyaaaaa727@gmail.com>
2025-09-01 10:38:19 +0530

    Time: 0 ms (100%), Space: 12.1 MB (31.88%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+        unordered_set<int> seen;
+        for (auto& [key, count] : freq) {
+            if (seen.count(count))
+                return false;
+            seen.insert(count);
+        }
+        return true;
+    }
+};
