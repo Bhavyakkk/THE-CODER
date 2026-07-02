3bd5c91bb5f1ee4ccf137d9dc817a6167089242f
THE CODER <bhavyaaaaa727@gmail.com>
2026-07-02 17:45:27 +0530

    Time: 0 ms (100%), Space: 97.5 MB (24.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int buyersprice=prices[0];
         int profit=0;
 
-        for(int i=1; i<prices.size(); i++){
+        for(int i=0; i<prices.size(); i++){
             if(prices[i]<buyersprice){
                 buyersprice=prices[i];
             }
