75f00dea22bd89179b7cf4a43e779208603d5eba
NOVA <bhavyaaaaa727@gmail.com>
2026-09-02 17:48:56 +0530

    Time: 850 ms (14.88%), Space: 11.7 MB (29.18%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -4,31 +4,29 @@ public:
     bool search(vector<vector<char>>& board, string word,
                 int i, int j, int index) {
 
-        // If entire word is found
+        
         if (index == word.size())
             return true;
 
-        // Out of bounds
+        
         if (i < 0 || i >= board.size() ||
             j < 0 || j >= board[0].size())
             return false;
 
-        // Character doesn't match
         if (board[i][j] != word[index])
             return false;
 
-        // Mark as visited
         char ch = board[i][j];
         board[i][j] = '#';
 
-        // Check 4 directions
+        
         bool ans =
-            search(board, word, i + 1, j, index + 1) ||  // down
-            search(board, word, i - 1, j, index + 1) ||  // up
-            search(board, word, i, j + 1, index + 1) ||  // right
-            search(board, word, i, j - 1, index + 1);    // left
+            search(board, word, i + 1, j, index + 1) ||  
+            search(board, word, i - 1, j, index + 1) || 
+            search(board, word, i, j + 1, index + 1) ||  
+            search(board, word, i, j - 1, index + 1);    
 
-        // Undo / backtrack
+        
         board[i][j] = ch;
 
         return ans;
